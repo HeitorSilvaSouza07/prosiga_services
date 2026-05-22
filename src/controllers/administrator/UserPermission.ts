e941241901584b0ac3b3076a1b329ad2994ed1c2
HeitorSilvaSouza77 <euclidesde@gmail.com>
2026-05-22 17:16:07 -0300

    feat: listbyuser permissions

--- src/controllers/administrator/UserPermission.ts
+++ src/controllers/administrator/UserPermission.ts
@@ -65,5 +65,35 @@ export class UserPermissionController{
         }
     }
 
+    static async listByUser(req: Request, res: Response){
+        try{
+
+            const id = Number(req.params.id);
+
+            const repo = Connection.getRepository(UserPermission);
+
+            const userPermissions = await repo.findBy({ IdUser: id})
+
+            if(!userPermissions){
+                return res.status(404).json({
+                    status: false,
+                    msg: 'Não existem permissoes criadas para esse usuario'
+                })
+            }
+
+            return res.status(200).json({
+                status: true, 
+                msg: 'Permissões encontradas',
+                data: userPermissions 
+            })
+
+        }catch(error: any){
+            return res.status(500).json({
+                status: false,
+                msg: 'Erro interno'
+            })
+        }
+    }
+
 
 }
