c88407c390d6193ab6fbb43365392ef1a6777eaf
HeitorSilvaSouza77 <euclidesde@gmail.com>
2026-05-22 15:08:15 -0300

    feat: create get controller in user permission

---
+++ src/controllers/administrator/UserPermission.ts
@@ -0,0 +1,69 @@
+import { Request, Response } from "express";
+import { UserPermission } from "../../entities/UserPermission";
+import { Connection } from "../../database/dataBase";
+export class UserPermissionController{
+    static async get(req: Request, res: Response){
+        try{
+
+            const id = Number(req.params.id);
+
+            const repo = Connection.getRepository(UserPermission);
+
+            const userPermission = await repo.findOneBy({ IdUp: id })
+
+            if(!userPermission){
+                return res.status(404).json({
+                    status : false,
+                    msg: 'Permissão não encontrada'
+                })
+            }
+
+            return res.status(200).json({
+                status: true,
+                msg: 'Permissão encontrada',
+                data: userPermission
+            })
+
+        }catch(error: any){
+            res.status(500).json({
+                status: false,
+                msg: error
+            })
+        }
+    }
+
+    static async update(req: Request, res: Response){
+        try{
+
+        }catch(error: any){
+            res.status(500).json({
+                status: false,
+                msg: error
+            })
+        }
+    }
+
+    static async create(req: Request, res: Response){
+        try{
+
+        }catch(error: any){
+            res.status(500).json({
+                status: false,
+                msg: error
+            })
+        }
+    }
+
+    static async list(req: Request, res: Response){
+        try{
+
+        }catch(error: any){
+            res.status(500).json({
+                status: false,
+                msg: error
+            })
+        }
+    }
+
+
+}
