0c12705d92878e0932c220a3c26c59f70275990c
HeitorSilvaSouza77 <euclidesde@gmail.com>
2026-06-05 11:09:01 -0300

    feat: endpoint create user auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,10 @@
 ﻿import { Router } from "express";
 import { SessionController } from "../controllers/auth/SessionController";
+import { CreateController } from "../controllers/auth/CreateController";
 
 const router = Router();
 
 router.post("/login", SessionController.loginUser);
+router.post("/create", CreateController.create);
 
 export default router;
